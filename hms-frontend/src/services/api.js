f9ffff791499c6457a168e40fb444cbaff399191
Riddhi Dabadghao <dabadghao.riddhi@gmail.com>
2026-05-27 03:22:58 +0530

    Fixed extra brace in api

--- hms-frontend/src/services/api.js
+++ hms-frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-
-baseURL: "https://hms-backend-rx8v.onrender.com"
-
+  baseURL: "https://hms-backend-rx8v.onrender.com"
 });
 
 export default API;
