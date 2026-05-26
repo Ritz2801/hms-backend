67bbe3001eac3f307330e3e4bcff46644458df52
Riddhi Dabadghao <dabadghao.riddhi@gmail.com>
2026-05-27 03:12:59 +0530

    Fixed API URL syntax

--- hms-frontend/src/services/api.js
+++ hms-frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "baseURL:"https://hms-backend-rx8v.onrender.com""
+
+baseURL: "https://hms-backend-rx8v.onrender.com"
+
 });
 
 export default API;
